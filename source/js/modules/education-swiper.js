2d6a1170056cd03e9a77c10e132875c7f889299f
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-12-15 01:23:54 +0300

    добавляет слайдер с тернерами

--- source/js/modules/education-swiper.js
+++ source/js/modules/education-swiper.js
@@ -4,18 +4,22 @@ const educationSwiper = new Swiper('.education__swiper', {
   breakpoints: {
     320: {
       slidesPerView: 1,
+      initialSlide: 2,
     },
     768: {
       slidesPerView: 3,
       spaceBetween: 20,
+      initialSlide: 0,
     },
     1024: {
       slidesPerView: 3,
       spaceBetween: 20,
+      initialSlide: 0,
     },
     1200: {
       slidesPerView: 4,
       spaceBetween: 20,
+      initialSlide: 0,
     },
   },
 
