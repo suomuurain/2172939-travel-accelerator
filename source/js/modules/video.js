1167a4af5270a1d80f4e1a0e7266a38b077df03f
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-12-20 21:53:10 +0300

    изменяет видео

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -1,4 +1,4 @@
-const playButton = document.querySelector('[data-video-play]');
+const playButton = document.querySelector('[data-play-button]');
 const video = document.querySelector('[data-video]');
 const poster = document.querySelector('[data-poster]');
 const swiperPagination = document.querySelector('.hero__swiper-pagination');
@@ -6,14 +6,14 @@ const swiperPagination = document.querySelector('.hero__swiper-pagination');
 const initVideo = () => {
   playButton.addEventListener('click', () => {
     poster.classList.add('is-hidden');
-    playButton.classList.add('is-hidden--else');
+    playButton.classList.add('is-hidden', 'is-hidden--else');
     video.src = 'https://www.youtube.com/embed/9TZXsZItgdw?rel=0&showinfo=0&autoplay=1';
   });
 };
 
 const disableVideo = () => {
   poster.classList.remove('is-hidden');
-  playButton.classList.remove('is-hidden--else');
+  playButton.classList.remove('is-hidden', 'is-hidden--else');
   video.src = '';
 };
 
